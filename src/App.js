362c7026e5f6c97186293368fce209f3a415c0d7
NikolajRFA <nrfadk@gmail.com>
2023-11-22 23:03:09 +0100

    Changes by laust

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./NavBar";
+import TitleIdCard from "./TitleCard";
+import {Dropdown} from "react-bootstrap";
+import DropdownTest from "./DropdownTest";
+
 
 function App() {
   return (
