81a0fe7188f86c82b3f77682a14941ef02b4ce08
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2023-11-30 22:48:23 +0100

    Started on episode sub-call

--- src/components/title/EpisodeEntry.jsx
+++ src/components/title/EpisodeEntry.jsx
@@ -1,12 +1,48 @@
 import Card from "react-bootstrap/Card";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Col, Row, Image} from "react-bootstrap";
+import LoadingSpinner from "../LoadingSpinner";
 
 export default function EpisodeEntry({episode}) {
+    const [episodeData, setEpisodeData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get(episode.episodeUrl)
+            .then(res => {
+                setEpisodeData(res.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
 
     return (
         <Card className="p-1 m-2">
             <Card.Body>
-                <Card.Title>{episode.title}</Card.Title>
-                <Card.Subtitle>Episode {episode.episode}</Card.Subtitle>
+                <Row>
+                    <Col>
+                        {!loading ? <Image src={episodeData.poster} width="100px"/> : <LoadingSpinner/>}
+                    </Col>
+                    <Col>
+                        <Card.Title><a href={episode.episodeUrl}>{episode.title}</a></Card.Title>
+                        <Card.Subtitle>Episode {episode.episode}</Card.Subtitle>
+                    </Col>
+                </Row>
+                {/*<Row>
+                    <Col>
+                        <Card.Title><a href={episode.episodeUrl}>{episode.title}</a></Card.Title>
+                        <Card.Subtitle>Episode {episode.episode}</Card.Subtitle>
+                    </Col>
+                    <Col>
+                        <Card.Text>{episodeData.plot}</Card.Text>
+                    </Col>
+                </Row>*/}
+
             </Card.Body>
         </Card>
     )
--- src/components/title/Season.jsx
+++ src/components/title/Season.jsx
@@ -1,8 +1,9 @@
-import {Accordion, Col, Row} from "react-bootstrap";
+import {Accordion, Button, Col, Row} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import EpisodeEntry from "./EpisodeEntry";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import LoadingSpinner from "../LoadingSpinner";
 
 export default function Season({seasonNumber, episodesUrl}) {
     const [episodes, setEpisodes] = useState([]);
@@ -11,6 +12,33 @@ export default function Season({seasonNumber, episodesUrl}) {
     const [error, setError] = useState(null);
 
     const [pageNo, setPageNo] = useState(0);
+    const [prevPage, setPrevPage] = useState(false);
+    const [nextPage, setNextPage] = useState(true);
+
+    function handleNextPage() {
+        setPrevPage(true);
+        const nextPageNo = pageNo + 1;
+        setPageNo(nextPageNo);
+        if (nextPageNo + 1 === pagingMetaData.numberOfPages) {
+            setNextPage(false);
+        }
+    }
+
+    function handlePrevPage() {
+        setNextPage(true);
+        const prevPageNo = pageNo - 1
+        setPageNo(prevPageNo);
+        if (prevPageNo + 1 === 1) {
+            setPrevPage(false);
+        }
+    }
+
+    const buttonStyle = {
+        width: '75px',
+        backgroundColor: '#FFE920',
+        border: 'none',
+        color: "black"
+    }
 
     useEffect(() => {
         axios.get(`${episodesUrl}?page=${pageNo}&pageSize=10&season=${seasonNumber}`)
@@ -23,30 +51,42 @@ export default function Season({seasonNumber, episodesUrl}) {
                 setError(error);
                 setLoading(false);
             });
-    }, []);
+    }, [pageNo]);
 
     return (
-        <Accordion.Item eventKey={seasonNumber}>
-            <Accordion.Header>
-                Season {seasonNumber}
-            </Accordion.Header>
-            <Accordion.Body>
-                <Card>
-                    <Card.Body>
-                        <Row xs={1} md={2} lg={4}>
-                            {episodes.map(episode => (
-                                <Col>
-                                    <EpisodeEntry episode={episode}/>
-                                </Col>
-                            ))}
-                        </Row>
+        <div>
+            {!loading
+                ? <Accordion.Item eventKey={seasonNumber}>
+                    <Accordion.Header>
+                        Season {seasonNumber}
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <Card>
+                            <Card.Body>
+                                <Row xs={1} md={2} lg={4}>
+                                    {episodes.map(episode => (
+                                        <Col>
+                                            <EpisodeEntry episode={episode}/>
+                                        </Col>
+                                    ))}
+                                </Row>
 
-                    </Card.Body>
-                    <Card.Footer>
-
-                    </Card.Footer>
-                </Card>
-            </Accordion.Body>
-        </Accordion.Item>
+                            </Card.Body>
+                            <Card.Footer className="text-end">
+                                <p>Page {pageNo + 1} of {pagingMetaData.numberOfPages}</p>
+                                <Button className="mx-2" style={buttonStyle} onClick={handlePrevPage}
+                                        disabled={!prevPage}>
+                                    Prev
+                                </Button>
+                                <Button style={buttonStyle} onClick={handleNextPage} disabled={!nextPage}>
+                                    Next
+                                </Button>
+                            </Card.Footer>
+                        </Card>
+                    </Accordion.Body>
+                </Accordion.Item>
+                : <LoadingSpinner/>
+            }
+        </div>
     )
 }
