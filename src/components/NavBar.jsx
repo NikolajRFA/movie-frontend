b6705379560a4b6b65eb207cfd95ebb54d7bfdf6
lanoth <78845859+lanoth@users.noreply.github.com>
2023-11-30 15:14:10 +0100

    Fixed import statement

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import Form from "react-bootstrap/Form";
 import {useEffect, useRef, useState} from "react";
 import {Dropdown, Image} from "react-bootstrap";
 import DropdownCard from "./DropdownCard";
-import SignInModal from "./components/LoginComp/SignInModal";
+import SignInModal from "./LoginComp/SignInModal";
 import axios from "axios";
 
 function NavBar() {
