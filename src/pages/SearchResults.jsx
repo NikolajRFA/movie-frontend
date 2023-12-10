668428da862f4dd149abc43797400e440c1a7f99
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2023-12-10 20:26:37 +0100

    Added paging to search results page

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -4,17 +4,49 @@ import Container from "react-bootstrap/Container";
 import {useEffect, useState} from "react";
 import SearchResultsObj from "../data_objects/SearchResultsObj";
 import LoadingSpinner from "../components/LoadingSpinner";
+import {Button, Col, Row} from "react-bootstrap";
 
 export default function SearchResults() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [results, setResults] = useState(() => new SearchResultsObj())
+// States relating to paging
+    const [pageNo, setPageNo] = useState(0);
+    const [prevPage, setPrevPage] = useState(false);
+    const [nextPage, setNextPage] = useState(true);
+
+    const buttonStyle = {
+        width: '75px',
+        backgroundColor: '#FFE920',
+        border: 'none',
+        color: "black"
+    }
+
+    function handleNextPage() {
+        //results.loading = true;
+        setPrevPage(true);
+        const nextPageNo = pageNo + 1;
+        setPageNo(nextPageNo);
+        if (nextPageNo + 1 === results.data.numberOfPages) {
+            setNextPage(false);
+        }
+    }
+
+    function handlePrevPage() {
+        //results.loading = true;
+        setNextPage(true);
+        const prevPageNo = pageNo - 1
+        setPageNo(prevPageNo);
+        if (prevPageNo + 1 === 1) {
+            setPrevPage(false);
+        }
+    }
 
     useEffect(() => {
         if (searchParams.has('q')) {
-            SearchResultsObj.fetchResults(searchParams.get('q'), 0, 5)
+            SearchResultsObj.fetchResults(searchParams.get('q'), pageNo, 5)
                 .then(res => setResults(res));
         }
-    }, [searchParams.get('q')]);
+    }, [searchParams.get('q'), pageNo]);
 
     return (
         searchParams.has('q')
@@ -26,6 +58,17 @@ export default function SearchResults() {
                         <SearchResultCard resultTitle={title}/>
                     </div>)
                     : <LoadingSpinner/>}
+            <Row className='text-end'>
+                <Col>
+                    <p>Page {pageNo + 1} of {!results.loading ? results.data.numberOfPages : <LoadingSpinner/>}</p>
+                    <Button className="mx-2" style={buttonStyle} onClick={handlePrevPage} disabled={!prevPage}>
+                        Prev
+                    </Button>
+                    <Button style={buttonStyle} onClick={handleNextPage} disabled={!nextPage}>
+                        Next
+                    </Button>
+                </Col>
+            </Row>
             </div>
             : <h1>Search for movies using the search bar!</h1>
     )
