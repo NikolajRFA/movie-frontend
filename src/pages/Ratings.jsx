091cdad34b5a6b526085c007cc7bad244abea411
NikolajRFA <nrfadk@gmail.com>
2023-12-18 13:26:37 +0100

    Added paging to user ratings

--- src/pages/Ratings.jsx
+++ src/pages/Ratings.jsx
@@ -4,6 +4,8 @@ import Card from "react-bootstrap/Card";
 import {useEffect, useState} from "react";
 import RatingListObj from "#data_objects/RatingListObj";
 import UserRatingCard from "#components/rating/UserRatingCard";
+import Paging from "#components/Paging";
+import LoadingSpinner from "#components/LoadingSpinner";
 
 export default function Ratings() {
     const [ratings, setRatings] = useState(() => new RatingListObj());
@@ -18,6 +20,24 @@ export default function Ratings() {
         fetchRatings();
     }, []);
 
+    function handleNextPage() {
+        const fetchRatings = async () => {
+            let newRatings = await RatingListObj.getNext(ratings);
+            setRatings(newRatings);
+        }
+        fetchRatings();
+        setPageNo(pageNo + 1);
+    }
+
+    function handlePrevPage() {
+        const fetchRatings = async () => {
+            let newRatings = await RatingListObj.getPrev(ratings);
+            setRatings(newRatings);
+        }
+        fetchRatings();
+        setPageNo(pageNo - 1);
+    }
+
     return (
         <div style={{display: 'flex'}}>
             <Sidebar/>
@@ -28,18 +48,24 @@ export default function Ratings() {
                 flex: 2,
                 marginTop: '75px',
             }}>
-                <Card style={{width: '300px'}}>
-                    <Card.Body>
-                        <Card.Title>
-                            Your Rated titles
-                        </Card.Title>
-                        <Card.Text>
-                            {!ratings.loading && ratings.data.items.length > 0
-                                ? ratings.data.items.map(item => <UserRatingCard key={item.data.tconst} rating={item}/>)
-                                : <p>You have not rated any titles yet!</p>}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                {!ratings.loading
+                    ? <Card style={{width: '300px'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                Your Rated titles
+                            </Card.Title>
+                            <Card.Text>
+                                {ratings.data.items.length > 0
+                                    ? ratings.data.items.map(item => <UserRatingCard key={item.data.tconst}
+                                                                                     rating={item}/>)
+                                    : <p>You have not rated any titles yet!</p>}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Paging listObj={ratings} onNext={handleNextPage} onPrev={handlePrevPage}/>
+                        </Card.Footer>
+                    </Card>
+                : <LoadingSpinner/>}
             </div>
         </div>
     )
